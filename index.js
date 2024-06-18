7e8ac8c3d3b9fb8cb4e74ee63f3336a42f79e4bc
Dorcas Egbende <dorcasegbende@gmail.com>
2024-06-18 13:00:19 +0200

    addanddisplayurl

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const shortid = require('shortid');
+const QRCode = require('qrcode');
+
+const app = express();
+
+
+let urlData = [];// Variable pour stocker les URLs en mémoire
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.render('index', { urls: urlData });
+});
+
+app.post('/shorten', async (req, res) => {
+  const fullUrl = req.body.fullUrl;
+  const shortUrl = shortid.generate();
+  const qrCodeUrl = await QRCode.toDataURL(`http://localhost:3000/${shortUrl}`);
+
+  urlData.push({ full: fullUrl, short: shortUrl, qrCode: qrCodeUrl });
+  res.redirect('/');
+});
+
+app.get('/:shortUrl', (req, res) => {
+  const shortUrl = req.params.shortUrl;
+  const url = urlData.find(url => url.short === shortUrl);
+  if (url) {
+    res.redirect(url.full);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+const port=3000;
+app.listen(port, () => {
+  console.log('Le serveur est lancé sur le port 3000');
+  console.log(`http://localhost:${port}`);
+});
