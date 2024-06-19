add4e7fc81c283bc390df1bd7fe4e4a928aa2a4a
Dorcas Egbende <dorcasegbende@gmail.com>
2024-06-19 19:32:29 +0200

    color

--- public/js/deleteUrl.js
+++ public/js/deleteUrl.js
@@ -1,18 +1,3 @@
-/*async function deleteUrl(shortUrl) {
-    try {
-      const response = await fetch(`/delete/${shortUrl}`, {
-        method: 'DELETE',
-      });
-  
-      if (response.ok) {
-        document.getElementById(`url-${shortUrl}`).remove();
-      } else {
-        console.error('erreur de la suppression');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  }*/
 
   const deleteButton=document.getElementById('delete');
   deleteButton.addEventListener('click', async (event) => {
